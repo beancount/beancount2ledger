beancount/beancount2ledger

commit 7431e5b0641348c3d6f9b12b36c029b8a0b035a6

Add rounding postings only when required
Ledger and beancount handle rounding and precision differently.
In ledger (and hledger), the entry with the most precision (in
the whole file) is taken for a currency.  This means that an
unrelated transaction can influence whether we need a rounding
entry for a transaction.

To solve this, we build up a dcontext based on all entries and
use that to format amounts -- if the formatted amount of a
rounding posting is zero, we don't have to add it (and in fact
adding it might break ledger).

However, for costs and prices we always have to retain the full
precision.

Fixes #9

beancount2ledger/__init__.py
@@ -9,6 +9,8 @@ Beancount to Ledger converter
 __license__ = "GPL-2.0-or-later"
 
 from beancount import loader
+from beancount.core import display_context
+from beancount.core.data import filter_txns
 from pkg_resources import DistributionNotFound
 from pkg_resources import get_distribution
 
@@ -21,22 +23,30 @@ except DistributionNotFound:
     __version__ = "undistributed"
 
 
-def convert(entries, output_format="ledger", config={}):
+def convert(entries, output_format="ledger", dcontext=None, config={}):
     """
     Convert beancount entries to ledger output
     """
 
+    if not dcontext:
+        dcontext = display_context.DisplayContext()
+        for entry in filter_txns(entries):
+            for posting in entry.postings:
+                if not posting.units:
+                    continue
+                dcontext.update(posting.units.number, posting.units.currency)
+
     if output_format == "hledger":
-        printer = HLedgerPrinter(config=config)
+        printer = HLedgerPrinter(dcontext=dcontext, config=config)
     else:
-        printer = LedgerPrinter(config=config)
+        printer = LedgerPrinter(dcontext=dcontext, config=config)
     return '\n'.join(printer(entry) for entry in entries)
 
 
-def convert_file(file, output_format="ledger", config={}):
+def convert_file(file, output_format="ledger", dcontext=None, config={}):
     """
     Convert beancount file to ledger output
     """
 
     entries, _, __ = loader.load_file(file)
-    return convert(entries, output_format, config=config)
+    return convert(entries, output_format, dcontext=dcontext, config=config)

beancount2ledger/hledger.py
@@ -11,8 +11,10 @@ __license__ = "GPL-2.0-or-later"
 import datetime
 
 from beancount.core.amount import Amount
+from beancount.core import amount
 from beancount.core import position
 from beancount.core import interpolate
+from beancount.core import display_context
 
 from .common import ROUNDING_ACCOUNT
 from .common import ledger_flag, ledger_str, quote_currency, user_meta
@@ -30,7 +32,6 @@ class HLedgerPrinter(LedgerPrinter):
         if val is None:
             return f"{key}:"
         return f"{key}: {val}"
-
 
     def Transaction(self, entry):
         # Insert a posting to absorb the residual if necessary. This is
@@ -89,13 +90,24 @@ class HLedgerPrinter(LedgerPrinter):
             posting.flag) else ''
         flag_posting = f"{flag}{posting.account}"
 
-        pos_str = (position.to_string(posting, self.dformat, detail=False)
-                   if isinstance(posting.units, Amount) else '')
-        if pos_str:
-            # Convert the cost as a price entry, that's what HLedger appears to want.
-            pos_str = pos_str.replace('{', '@ ').replace('}', '')
+        pos_str = ''
+        # We don't use position.to_string() because that uses the same
+        # dformat for amount and cost, but we want dformat from our
+        # dcontext to format amounts to the right precision while
+        # retaining the full rpecision for costs.
+        if isinstance(posting.units, Amount):
+            pos_str = posting.units.to_string(self.dformat)
+            # Don't create a posting if the amount (rounded to the display
+            # precision) is 0.00.
+            amt = amount.from_string(pos_str)
+            if not amt:
+                return
+        # Convert the cost as a price entry, that's what HLedger appears to want.
+        if isinstance(posting.cost, position.Cost):
+            pos_str += ' @ ' + position.cost_to_str(
+                posting.cost, display_context.DEFAULT_FORMATTER, detail=False)
 
-        price_str = ('@ {}'.format(posting.price.to_string(self.dformat_max))
+        price_str = ('@ {}'.format(posting.price.to_string())
                      if posting.price is not None and posting.cost is None else
                      '')
         if posting.meta and '__automatic__' in posting.meta and not '__residual__' in posting.meta:

beancount2ledger/ledger.py
@@ -35,8 +35,6 @@ class LedgerPrinter:
         self.dcontext = dcontext or display_context.DEFAULT_DISPLAY_CONTEXT
         self.dformat = self.dcontext.build(
             precision=display_context.Precision.MOST_COMMON)
-        self.dformat_max = self.dcontext.build(
-            precision=display_context.Precision.MAXIMUM)
         self.config = set_default(config)
 
     def __call__(self, obj):
@@ -136,11 +134,25 @@ class LedgerPrinter:
             posting.flag) else ''
         flag_posting = f"{flag}{posting.account}"
 
+        pos_str = ''
+        # We don't use position.to_string() because that uses the same
+        # dformat for amount and cost, but we want dformat from our
+        # dcontext to format amounts to the right precision while
+        # retaining the full rpecision for costs.
+        if isinstance(posting.units, Amount):
+            pos_str = posting.units.to_string(self.dformat)
+            # Don't create a posting if the amount (rounded to the display
+            # precision) is 0.00.
+            amt = amount.from_string(pos_str)
+            if not amt:
+                return
         # We can't use default=True, even though we're interested in the
         # cost details, but we have to add them ourselves in the format
         # expected by ledger.
-        pos_str = (position.to_string(posting, self.dformat, detail=False)
-                   if isinstance(posting.units, Amount) else '')
+        if isinstance(posting.cost, position.Cost):
+            pos_str += ' {' + position.cost_to_str(
+                posting.cost, display_context.DEFAULT_FORMATTER,
+                detail=False) + '}'
         if posting.cost:
             if posting.cost.date != entry.date:
                 pos_str += f" [{posting.cost.date}]"
@@ -148,8 +160,7 @@ class LedgerPrinter:
                 pos_str += f" ({posting.cost.label})"
 
         if posting.price is not None:
-            price_str = '@ {}'.format(
-                posting.price.to_string(self.dformat_max))
+            price_str = '@ {}'.format(posting.price.to_string())
         else:
             # Figure out if we need to insert a price on a posting held at cost.
             # See https://groups.google.com/d/msg/ledger-cli/35hA0Dvhom0/WX8gY_5kHy0J
@@ -159,8 +170,7 @@ class LedgerPrinter:
             cost = posting.cost
             if postings_at_price and postings_at_cost and cost:
                 price_str = '@ {}'.format(
-                    amount.Amount(cost.number,
-                                  cost.currency).to_string(self.dformat))
+                    amount.Amount(cost.number, cost.currency).to_string())
             else:
                 price_str = ''
 

tests/hledger_test.py
@@ -395,6 +395,85 @@ class TestHLedgerConversion(test_utils.TestCase):
                 Equity:Opening-Balance
         """, result)
 
+    @loader.load_doc()
+    def test_retain_precision(self, entries, _, ___):
+        """
+            2010-01-01 open Expenses:Nutrition:Food
+            2010-01-01 open Assets:Bank
+            2010-01-01 open Assets:Investments
+
+            2020-01-10 * "Supermarket"
+              Expenses:Nutrition:Food  150.75 THB @ 0.03310116086236 USD
+              Assets:Bank                                      -4.99 USD
+
+            2020-01-10 * "Supermarket"
+              Expenses:Nutrition:Food  150.75 THB @ 0.025207296849 GBP
+              Assets:Bank                                    -3.80 GBP
+
+            2020-02-28 * "Bought GB00BPN5P782"
+              Assets:Investments           1 GB00BPN5P782 {101.689996215 GBP}
+              Assets:Investments                                 -101.69 GBP
+        """
+        result = beancount2ledger.convert(entries, "hledger")
+        self.assertLines(r"""
+            account Expenses:Nutrition:Food
+
+            account Assets:Bank
+
+            account Assets:Investments
+
+            2020-01-10 * Supermarket
+              Expenses:Nutrition:Food                                        150.75 THB @ 0.03310116086236 USD
+              Assets:Bank                                                     -4.99 USD
+
+            2020-01-10 * Supermarket
+              Expenses:Nutrition:Food                                        150.75 THB @ 0.025207296849 GBP
+              Assets:Bank                                                     -3.80 GBP
+
+            2020-02-28 * Bought GB00BPN5P782
+              Assets:Investments                    1 "GB00BPN5P782" @ 101.689996215 GBP
+              Assets:Investments                                             -101.69 GBP
+        """, result)
+
+    @loader.load_doc()
+    def test_rounding(self, entries, _, ___):
+        """
+            2010-01-01 open Expenses:Test
+            2010-01-01 open Assets:Bank
+
+            2020-01-10 * "Test"
+              Expenses:Test                           4.990 USD
+              Assets:Bank                            -4.990 USD
+
+            2020-01-10 * "Test: will fail in ledger due to precision of USD"
+              Expenses:Test            150.75 THB @ 0.03344 USD
+              Assets:Bank                             -5.04 USD
+
+            2020-01-10 * "Test: will fail in ledger due to precision of USD"
+              Expenses:Test               140.1 THB @ 0.021 USD
+              Assets:Bank                             -2.94 USD
+        """
+        result = beancount2ledger.convert(entries, "hledger")
+        self.assertLines(r"""
+            account Expenses:Test
+
+            account Assets:Bank
+
+            2020-01-10 * Test
+              Expenses:Test                                                    4.990 USD
+              Assets:Bank                                                     -4.990 USD
+
+            2020-01-10 * Test: will fail in ledger due to precision of USD
+              Expenses:Test                                                   150.75 THB @ 0.03344 USD
+              Assets:Bank                                                     -5.040 USD
+              Equity:Rounding                                                 -0.001 USD
+
+            2020-01-10 * Test: will fail in ledger due to precision of USD
+              Expenses:Test                                                   140.10 THB @ 0.021 USD
+              Assets:Bank                                                     -2.940 USD
+              Equity:Rounding                                                 -0.002 USD
+        """, result)
+
     def test_example(self):
         """
         Test converted example with hledger

tests/ledger_test.py
@@ -217,7 +217,6 @@ class TestLedgerConversion(test_utils.TestCase):
             Assets:CA:Investment:HOOL           5 HOOL {520.0 USD} @ 520.0 USD
             Expenses:Commissions             9.95 USD
             Assets:CA:Investment:Cash    -2939.46 CAD @ 0.8879 USD
-            Equity:Rounding             -0.003466 USD
 
           P 2014-11-02 HOOL    520.0 USD
 
@@ -700,6 +699,85 @@ class TestLedgerConversion(test_utils.TestCase):
             2020-11-12=2020-11-03 * (1234) Code and aux-date
                 Assets:Test                                                   10.00 EUR
                 Equity:Opening-Balance
+        """, result)
+
+    @loader.load_doc()
+    def test_retain_precision(self, entries, _, ___):
+        """
+            2010-01-01 open Expenses:Nutrition:Food
+            2010-01-01 open Assets:Bank
+            2010-01-01 open Assets:Investments
+
+            2020-01-10 * "Supermarket"
+              Expenses:Nutrition:Food  150.75 THB @ 0.03310116086236 USD
+              Assets:Bank                                      -4.99 USD
+
+            2020-01-10 * "Supermarket"
+              Expenses:Nutrition:Food  150.75 THB @ 0.025207296849 GBP
+              Assets:Bank                                    -3.80 GBP
+
+            2020-02-28 * "Bought GB00BPN5P782"
+              Assets:Investments           1 GB00BPN5P782 {101.689996215 GBP}
+              Assets:Investments                                 -101.69 GBP
+        """
+        result = beancount2ledger.convert(entries, "ledger")
+        self.assertLines(r"""
+            account Expenses:Nutrition:Food
+
+            account Assets:Bank
+
+            account Assets:Investments
+
+            2020-01-10 * Supermarket
+              Expenses:Nutrition:Food                                        150.75 THB @ 0.03310116086236 USD
+              Assets:Bank                                                     -4.99 USD
+
+            2020-01-10 * Supermarket
+              Expenses:Nutrition:Food                                        150.75 THB @ 0.025207296849 GBP
+              Assets:Bank                                                     -3.80 GBP
+
+            2020-02-28 * Bought GB00BPN5P782
+              Assets:Investments                   1 "GB00BPN5P782" {101.689996215 GBP}
+              Assets:Investments                                            -101.69 GBP
+        """, result)
+
+    @loader.load_doc()
+    def test_rounding(self, entries, _, ___):
+        """
+            2010-01-01 open Expenses:Test
+            2010-01-01 open Assets:Bank
+
+            2020-01-10 * "Test"
+              Expenses:Test                           4.990 USD
+              Assets:Bank                            -4.990 USD
+
+            2020-01-10 * "Test: will fail in ledger due to precision of USD"
+              Expenses:Test            150.75 THB @ 0.03344 USD
+              Assets:Bank                             -5.04 USD
+
+            2020-01-10 * "Test: will fail in ledger due to precision of USD"
+              Expenses:Test               140.1 THB @ 0.021 USD
+              Assets:Bank                             -2.94 USD
+        """
+        result = beancount2ledger.convert(entries)
+        self.assertLines(r"""
+            account Expenses:Test
+
+            account Assets:Bank
+
+            2020-01-10 * Test
+              Expenses:Test                                                    4.990 USD
+              Assets:Bank                                                     -4.990 USD
+
+            2020-01-10 * Test: will fail in ledger due to precision of USD
+              Expenses:Test                                                   150.75 THB @ 0.03344 USD
+              Assets:Bank                                                     -5.040 USD
+              Equity:Rounding                                                 -0.001 USD
+
+            2020-01-10 * Test: will fail in ledger due to precision of USD
+              Expenses:Test                                                   140.10 THB @ 0.021 USD
+              Assets:Bank                                                     -2.940 USD
+              Equity:Rounding                                                 -0.002 USD
         """, result)
 
     def test_example(self):

commit cd9a4e157193bb8c107d6cb35d40ee3507b10077

Don't skip postings with 0.00 amounts
It makes sense to skip rounding entries that would be rendered
as 0.00, but we should not skip entries from users with 0.00
amounts as that might break some transactions.

This bug was introduced in commit 7431e5b06413 ("Add rounding postings
only when required").

beancount2ledger/__init__.py
@@ -32,7 +32,7 @@ def convert(entries, output_format="ledger", dcontext=None, config={}):
         dcontext = display_context.DisplayContext()
         for entry in filter_txns(entries):
             for posting in entry.postings:
-                if not posting.units:
+                if posting.units is None:
                     continue
                 if (posting.meta and '__automatic__' in posting.meta
                         and not '__residual__' in posting.meta):

beancount2ledger/common.py
@@ -193,10 +193,10 @@ def is_automatic_posting(posting):
     return False
 
 
-def filter_display_postings(entry, dformat):
+def filter_rounding_postings(entry, dformat):
     """
-    Return entry without postings that wouldn't be displayed because
-    the display precision rounds them to 0.00.
+    Return entry without rounding postings that wouldn't be displayed
+    because the display precision rounds them to 0.00.
     """
 
     postings = list(entry.postings)
@@ -206,7 +206,7 @@ def filter_display_postings(entry, dformat):
         # Don't create a posting if the amount (rounded to the display
         # precision) is 0.00.
         amt = amount.from_string(pos_str)
-        if amt:
+        if amt or posting.account != ROUNDING_ACCOUNT:
             new_postings.append(posting)
     entry = entry._replace(postings=new_postings)
     return entry

beancount2ledger/hledger.py
@@ -18,7 +18,7 @@ from beancount.core import display_context
 
 from .common import ROUNDING_ACCOUNT
 from .common import ledger_flag, ledger_str, quote_currency, user_meta
-from .common import get_lineno
+from .common import get_lineno, filter_rounding_postings
 from .ledger import LedgerPrinter
 
 
@@ -41,6 +41,9 @@ class HLedgerPrinter(LedgerPrinter):
         # a bug, so instead, we simply insert a rounding account to absorb the
         # residual and precisely balance the transaction.
         entry = interpolate.fill_residual_posting(entry, ROUNDING_ACCOUNT)
+        # Remove postings which wouldn't be displayed (due to precision
+        # rounding amounts to 0.00)
+        entry = filter_rounding_postings(entry, self.dformat)
 
         # Compute the string for the payee and narration line.
         strings = []
@@ -110,11 +113,6 @@ class HLedgerPrinter(LedgerPrinter):
         # retaining the full rpecision for costs.
         if isinstance(posting.units, Amount):
             pos_str = posting.units.to_string(self.dformat)
-            # Don't create a posting if the amount (rounded to the display
-            # precision) is 0.00.
-            amt = amount.from_string(pos_str)
-            if not amt:
-                return
         # Convert the cost as a price entry, that's what HLedger appears to want.
         if isinstance(posting.cost, position.Cost):
             pos_str += ' @ ' + position.cost_to_str(

beancount2ledger/ledger.py
@@ -22,7 +22,7 @@ from beancount.core import display_context
 
 from .common import ROUNDING_ACCOUNT
 from .common import ledger_flag, ledger_str, quote_currency, postings_by_type, user_meta
-from .common import set_default, get_lineno, is_automatic_posting, filter_display_postings
+from .common import set_default, get_lineno, is_automatic_posting, filter_rounding_postings
 
 
 class LedgerPrinter:
@@ -84,7 +84,7 @@ class LedgerPrinter:
         entry = interpolate.fill_residual_posting(entry, ROUNDING_ACCOUNT)
         # Remove postings which wouldn't be displayed (due to precision
         # rounding amounts to 0.00)
-        entry = filter_display_postings(entry, self.dformat)
+        entry = filter_rounding_postings(entry, self.dformat)
 
         # Compute the string for the payee and narration line.
         strings = []
@@ -178,7 +178,7 @@ class LedgerPrinter:
             (postings_simple, _, __) = postings_by_type(entry)
             postings_no_amount = [
                 posting for posting in postings_simple
-                if not posting.units or is_automatic_posting(posting)
+                if posting.units is None or is_automatic_posting(posting)
             ]
             cost = posting.cost
             if cost and not postings_no_amount and len(entry.postings) > 2:

tests/hledger_test.py
@@ -557,6 +557,30 @@ class TestHLedgerConversion(test_utils.TestCase):
               Equity:Opening-Balance                                         -70.00 EUR
         """, result)
 
+    @loader.load_doc()
+    def test_keep_zero_amounts(self, entries, _, ___):
+        """
+            2010-01-01 open Assets:Investments
+            2010-01-01 open Expenses:Fees:Investments
+
+            2020-05-24 * "Bought ETH"
+              Assets:Investments                     0.00000221 ETH {190.60 EUR}
+              Expenses:Fees:Investments                                 0.00 EUR
+              Assets:Investments                                       -0.00 EUR
+        """
+        result = beancount2ledger.convert(entries, "hledger")
+        self.assertLines(r"""
+            account Assets:Investments
+
+            account Expenses:Fees:Investments
+
+            2020-05-24 * Bought ETH
+              Assets:Investments                            0.00000221 ETH @ 190.60 EUR
+              Expenses:Fees:Investments                                        0.00 EUR
+              Assets:Investments                                               0.00 EUR
+        """, result)
+
+
     def test_example(self):
         """
         Test converted example with hledger

tests/ledger_test.py
@@ -863,6 +863,29 @@ class TestLedgerConversion(test_utils.TestCase):
               Equity:Opening-Balance                                         -70.00 EUR
         """, result)
 
+    @loader.load_doc()
+    def test_keep_zero_amounts(self, entries, _, ___):
+        """
+            2010-01-01 open Assets:Investments
+            2010-01-01 open Expenses:Fees:Investments
+
+            2020-05-24 * "Bought ETH"
+              Assets:Investments                     0.00000221 ETH {190.60 EUR}
+              Expenses:Fees:Investments                                 0.00 EUR
+              Assets:Investments                                       -0.00 EUR
+        """
+        result = beancount2ledger.convert(entries)
+        self.assertLines(r"""
+            account Assets:Investments
+
+            account Expenses:Fees:Investments
+
+            2020-05-24 * Bought ETH
+              Assets:Investments                            0.00000221 ETH {190.60 EUR} @ 190.60 EUR
+              Expenses:Fees:Investments                                        0.00 EUR
+              Assets:Investments                                               0.00 EUR
+        """, result)
+
     def test_example(self):
         with tempfile.NamedTemporaryFile('w',
                                          suffix='.beancount',