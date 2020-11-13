beancount/beancount2ledger

commit 27dcd7eec06b40514f550179e7d5cc253907f9bc

Show amounts only when they are specified in the source file
Don't show amounts that were added through interpolation.  This
way, the conversion is a more faithful representation of the
original.

Fixes #10

beancount2ledger/hledger.py
@@ -62,12 +62,14 @@ class HLedgerPrinter(LedgerPrinter):
         price_str = ('@ {}'.format(posting.price.to_string(self.dformat_max))
                      if posting.price is not None and posting.cost is None else
                      '')
-
-        # Width we have available for the amount: take width of
-        # flag_posting add 2 for the indentation of postings and
-        # add 2 to separate account from amount
-        len_amount = max(0, 76 - (len(flag_posting) + 2 + 2))
-        posting_str = f'  {flag_posting}  {quote_currency(pos_str):>{len_amount}} {quote_currency(price_str)}'
+        if posting.meta and '__automatic__' in posting.meta and not '__residual__' in posting.meta:
+            posting_str = f'  {flag_posting}'
+        else:
+            # Width we have available for the amount: take width of
+            # flag_posting add 2 for the indentation of postings and
+            # add 2 to separate account from amount
+            len_amount = max(0, 76 - (len(flag_posting) + 2 + 2))
+            posting_str = f'  {flag_posting}  {quote_currency(pos_str):>{len_amount}} {quote_currency(price_str)}'
         self.io.write(posting_str.rstrip())
 
         self.io.write('\n')

beancount2ledger/ledger.py
@@ -157,11 +157,14 @@ class LedgerPrinter:
             else:
                 price_str = ''
 
-        # Width we have available for the amount: take width of
-        # flag_posting add 2 for the indentation of postings and
-        # add 2 to separate account from amount
-        len_amount = max(0, 75 - (len(flag_posting) + 2 + 2))
-        posting_str = f'  {flag_posting}  {quote_currency(pos_str):>{len_amount}} {quote_currency(price_str)}'
+        if posting.meta and '__automatic__' in posting.meta and not '__residual__' in posting.meta:
+            posting_str = f'  {flag_posting}'
+        else:
+            # Width we have available for the amount: take width of
+            # flag_posting add 2 for the indentation of postings and
+            # add 2 to separate account from amount
+            len_amount = max(0, 75 - (len(flag_posting) + 2 + 2))
+            posting_str = f'  {flag_posting}  {quote_currency(pos_str):>{len_amount}} {quote_currency(price_str)}'
         self.io.write(posting_str.rstrip())
         self.io.write('\n')
 

tests/hledger_test.py
@@ -78,19 +78,19 @@ class TestHLedgerConversion(test_utils.TestCase):
 
           2020-01-01 * Grocery | Salad
             Expenses:Grocery                                                 5 SALAD @ 1.00 USD
-            Assets:Bank                                                             -5.00 USD
+            Assets:Bank
 
           2020-01-01 * Grocery | Salad
             Expenses:Grocery                                                 5 SALAD @ 1.00 USD
-            Assets:Bank                                                             -5.00 USD
+            Assets:Bank
 
           2020-01-01 * Grocery | Salad
             Expenses:Grocery                                                 5 SALAD @ 1.00 USD
-            Assets:Bank                                                             -5.00 USD
+            Assets:Bank
 
           2020-01-01 * Grocery | Salad
             Expenses:Grocery                                                 5 SALAD @ 1.00 USD
-            Assets:Bank                                                             -5.00 USD
+            Assets:Bank
         """, result)
 
     def test_example(self):

tests/ledger_test.py
@@ -186,7 +186,7 @@ class TestLedgerConversion(test_utils.TestCase):
 
           2014-03-02 * Something
             Expenses:Restaurant                                                     50.02 USD
-            Assets:Cash                                                            -50.02 USD
+            Assets:Cash
 
         """, result)
 
@@ -355,15 +355,15 @@ class TestLedgerConversion(test_utils.TestCase):
 
           2020-02-01 * Super Shop | New computer
             Expenses:Computers  1 COMPUTER {900.00 USD} [2019-12-25] (DiscountedComputer) @ 1100.00 USD
-            Assets:Bank                                                      -900.00 USD
+            Assets:Bank
 
           2020-02-02 * Super Shop | New computer
             Expenses:Computers           1 COMPUTER {900.00 USD} (DiscountedComputer) @ 1100.00 USD
-            Assets:Bank                                                      -900.00 USD
+            Assets:Bank
 
           2020-02-03 * Super Shop | New computer
             Expenses:Computers                      1 COMPUTER {900.00 USD} [2019-12-25] @ 1100.00 USD
-            Assets:Bank                                                      -900.00 USD
+            Assets:Bank
         """, result)
 
     @loader.load_doc()
@@ -388,6 +388,24 @@ class TestLedgerConversion(test_utils.TestCase):
         result = beancount2ledger.convert(entries)
         len_postings = [len(line) for line in result.rstrip().split('\n')]
         self.assertEqual(len_postings[-10:-1:2], [75, 75, 80, 98, 75])
+
+    @loader.load_doc()
+    def test_posting_no_amount(self, entries, _, ___):
+        """
+          2010-01-01 open Assets:Test
+
+          2020-07-25 txn "No amount on second posting"
+            Assets:Test                        10.00 EUR
+            Assets:Test
+        """
+        result = beancount2ledger.convert(entries)
+        self.assertLines("""
+          account Assets:Test
+
+          2020-07-25 * No amount on second posting
+            Assets:Test                        10.00 EUR
+            Assets:Test
+        """, result)
 
     @loader.load_doc()
     def test_price_alignment(self, entries, _, ___):

commit 283e011923eabf16116b7f92d8e3c41a7cdf9fdb

Avoid creating two or more postings with null amount
If a posting without an amount is given and several amounts would
be added when balancing, beancount will create several postings.
But we ignore the amount on those postings (since they were added
by beancount and not the user), which means we may end up with
two or more postings with no amount, which is not valid.

Therefore, only take *one* posting by looking at the line number.

This fixes a regression which was introduced in commit 27dcd7eec0
("Show amounts only when they are specified in the source file").

Fixes #23

beancount2ledger/common.py
@@ -9,6 +9,7 @@ Common functions
 __license__ = "GPL-2.0-or-later"
 
 import re
+import sys
 
 from beancount.core import data
 from beancount.core import convert
@@ -169,3 +170,12 @@ def user_meta(meta):
         'lineno',
     ]
     return {key: meta[key] for key in meta if key not in ignore}
+
+
+def get_lineno(posting):
+    """
+    Get line number of posting
+    """
+
+    meta = posting.meta or {}
+    return meta.get("lineno", sys.maxsize)

beancount2ledger/hledger.py
@@ -18,6 +18,7 @@ from beancount.core import display_context
 
 from .common import ROUNDING_ACCOUNT
 from .common import ledger_flag, ledger_str, quote_currency, user_meta
+from .common import get_lineno
 from .ledger import LedgerPrinter
 
 
@@ -81,7 +82,19 @@ class HLedgerPrinter(LedgerPrinter):
             if meta:
                 self.io.write(indent + f'; {meta}\n')
 
-        for posting in entry.postings:
+        # If a posting without an amount is given and several amounts would
+        # be added when balancing, beancount will create several postings.
+        # But we ignore the amount on those postings (since they were added
+        # by beancount and not the user), which means we may end up with
+        # two or more postings with no amount, which is not valid.
+        # Therefore, only take *one* posting by looking at the line number.
+        seen = set()
+        for posting in sorted(entry.postings, key=lambda p: get_lineno(p)):
+            lineno = get_lineno(posting)
+            if lineno is not None:
+                if lineno in seen:
+                    continue
+                seen.add(lineno)
             self.Posting(posting, entry)
 
     def Posting(self, posting, entry):

beancount2ledger/ledger.py
@@ -22,7 +22,7 @@ from beancount.core import display_context
 
 from .common import ROUNDING_ACCOUNT
 from .common import ledger_flag, ledger_str, quote_currency, postings_by_type, user_meta
-from .common import set_default
+from .common import set_default, get_lineno
 
 
 class LedgerPrinter:
@@ -123,7 +123,19 @@ class LedgerPrinter:
             if meta:
                 self.io.write(indent + f'; {formatted_meta}\n')
 
-        for posting in entry.postings:
+        # If a posting without an amount is given and several amounts would
+        # be added when balancing, beancount will create several postings.
+        # But we ignore the amount on those postings (since they were added
+        # by beancount and not the user), which means we may end up with
+        # two or more postings with no amount, which is not valid.
+        # Therefore, only take *one* posting by looking at the line number.
+        seen = set()
+        for posting in sorted(entry.postings, key=lambda p: get_lineno(p)):
+            lineno = get_lineno(posting)
+            if lineno is not None:
+                if lineno in seen:
+                    continue
+                seen.add(lineno)
             self.Posting(posting, entry)
 
     def Posting(self, posting, entry):

tests/hledger_test.py
@@ -511,6 +511,29 @@ class TestHLedgerConversion(test_utils.TestCase):
               Equity:Opening-Balance
         """, result)
 
+    @loader.load_doc()
+    def test_avoid_multiple_null_postings(self, entries, _, ___):
+        """
+            2010-01-01 open Assets:Cash
+            2010-01-01 open Equity:Opening-balance
+
+            2020-01-01 * "Opening balance: cash"
+              Assets:Cash                                               0.10 EUR
+              Assets:Cash                                               1.00 GBP
+              Equity:Opening-balance
+        """
+        result = beancount2ledger.convert(entries, "hledger")
+        self.assertLines(r"""
+            account Assets:Cash
+
+            account Equity:Opening-balance
+
+            2020-01-01 * Opening balance: cash
+              Assets:Cash                                                       0.10 EUR
+              Assets:Cash                                                       1.00 GBP
+              Equity:Opening-balance
+        """, result)
+
     def test_example(self):
         """
         Test converted example with hledger

tests/ledger_test.py
@@ -817,6 +817,29 @@ class TestLedgerConversion(test_utils.TestCase):
               Equity:Opening-Balance
         """, result)
 
+    @loader.load_doc()
+    def test_avoid_multiple_null_postings(self, entries, _, ___):
+        """
+            2010-01-01 open Assets:Cash
+            2010-01-01 open Equity:Opening-balance
+
+            2020-01-01 * "Opening balance: cash"
+              Assets:Cash                                               0.10 EUR
+              Assets:Cash                                               1.00 GBP
+              Equity:Opening-balance
+        """
+        result = beancount2ledger.convert(entries)
+        self.assertLines(r"""
+            account Assets:Cash
+
+            account Equity:Opening-balance
+
+            2020-01-01 * Opening balance: cash
+              Assets:Cash                                                       0.10 EUR
+              Assets:Cash                                                       1.00 GBP
+              Equity:Opening-balance
+        """, result)
+
     def test_example(self):
         with tempfile.NamedTemporaryFile('w',
                                          suffix='.beancount',